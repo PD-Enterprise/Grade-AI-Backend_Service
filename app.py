712d8d002b7baf23a7727810f2ba4956866c93c5
PD <89239979+PDgaming@users.noreply.github.com>
2025-01-27 19:07:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response, jsonify
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -31,4 +32,5 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__== "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    port = os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
